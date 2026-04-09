7c7bffa8721f9570981e478a49e287752c078e72
mohamed.g.ismail <mohamed.g.ismail@orange.com>
2026-04-09 21:21:56 +0200

    Add fixes

--- content/middleware.py
+++ content/middleware.py
@@ -9,11 +9,9 @@ class ForceAdminEnglishMiddleware:
 
     def __call__(self, request):
         if request.path.startswith("/super-admin/"):
-            translation.activate("en")
-            request.LANGUAGE_CODE = "en"
-            response = self.get_response(request)
-            response.set_cookie("django_language", "en")
-            return response
+            with translation.override("en"):
+                request.LANGUAGE_CODE = "en"
+                return self.get_response(request)
         return self.get_response(request)
 
 
@@ -22,6 +20,8 @@ class ForceArabicDefaultMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if request.path.startswith("/super-admin/"):
+            return self.get_response(request)
         if "django_language" not in request.COOKIES:
             translation.activate("ar")
             request.LANGUAGE_CODE = "ar"
